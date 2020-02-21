104e7a5fcb7b992ef62e4fb8550842a3d91f1392
William Stevens <wstevens.hornet@gmail.com>
2020-02-21 22:01:41 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,51 @@
 class UsersController < ApplicationController
 
+ get "/create_user" do
+    erb :create_user
+  end
+
+  post "/create_user" do
+    if params[:username]=="" || params[:password]==""
+      redirect to '/failure'
+    end
+
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+			redirect to "/login"
+		else
+			redirect to "/create_user"
+		end
+  end
+
+  get '/index' do
+    @user = User.find(session[:user_id])
+    erb :index
+  end
+
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+      user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/account'
+    else
+      redirect to '/failure'
+    end
+  end
+  
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 end
